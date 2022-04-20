836ae5375f95898f6f19a5b702035336c67280e0
augustoluiz <augustogu7@yahoo.com.br>
2022-04-20 01:41:31 -0300

    Connecting a node container to a MySQL container

--- dockerfile/example_9/node/index.js
+++ dockerfile/example_9/node/index.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const config = {
+    host: 'db',
+    user: 'root',
+    password: 'root',
+    database: 'nodedb'
+};
+const mysql = require('mysql')
+const connection = mysql.createConnection(config);
+
+const sql = `INSERT INTO people(name) values ('Augusto')`;
+connection.query(sql);
+connection.end();
 
 app.get('/', (req, res) => {
     res.send('<h1>Hello, World!</h1>')
